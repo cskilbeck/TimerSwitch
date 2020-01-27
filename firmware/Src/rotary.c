a10c1ac2a388c00ae6dfa3fefaca13206aa14783
Charlie <charlie>
2020-01-27 12:05:56 +0000

    decoder maybe

--- firmware/Src/rotary.c
+++ firmware/Src/rotary.c
@@ -1,11 +1,30 @@
 #include "main.h"
 
-int const rot_enc_table[16] = { 0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1, 1, 0 };
+// int const rot_enc_table[16] = { 0, 1, -1, 0, -1, 0, 0, 1, 1, 0, 0, -1, 0, -1, 1, 0 };
 
-int state = 0;
+// int state = 0;
+
+// int rotary_update()
+// {
+//     state = ((state << 2) | (~GPIOA->IDR & 0x3)) & 0xf;
+//     return rot_enc_table[state];
+// }
+
+static int const rot_enc_table[] = { 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 0 };
+static int state = 0;
+static uint8_t store = 0;
 
 int rotary_update()
 {
     state = ((state << 2) | (~GPIOA->IDR & 0x3)) & 0xf;
-    return rot_enc_table[state];
+    if(rot_enc_table[state] != 0) {
+        store = (store << 4) | state;
+        switch(store) {
+        case 0x17:
+            return 1;
+        case 0x2b:
+            return -1;
+        }
+    }
+    return 0;
 }
