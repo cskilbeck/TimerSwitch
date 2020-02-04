0877e4770bdff54a37fd5d9d2056a7e1b46f7121
Charlie <charlie>
2020-02-04 13:14:04 +0000

    fw: noodle

--- firmware/Src/user.cpp
+++ firmware/Src/user.cpp
@@ -7,21 +7,18 @@
 
 //////////////////////////////////////////////////////////////////////
 
+constexpr uintptr GPIO_PORT_A = 0x48000000UL;
+constexpr int     BUTTON_BIT = 9;
+
 volatile uint32 ticks = 0;
 volatile int    rotation = 0;
+int             number = 0;
+button_state_t  button_state;
 
-int number = 0;
-int display_number = 0;
-
-//////////////////////////////////////////////////////////////////////
-
-constexpr uintptr GPIO_PORT_A = 0x48000000UL;
-
-button_t<GPIO_PORT_A, 9, 32> button;
+button_t<GPIO_PORT_A, BUTTON_BIT, 32> button;
 
 //////////////////////////////////////////////////////////////////////
-
-// GPIO_PIN_9
+// 10KHz timer ISR
 
 extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 {
@@ -30,6 +27,7 @@ extern "C" void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 }
 
 //////////////////////////////////////////////////////////////////////
+// some activity on rotary encoder lines
 
 extern "C" void HAL_GPIO_EXTI_Callback(uint16 GPIO_Pin)
 {
@@ -37,69 +35,48 @@ extern "C" void HAL_GPIO_EXTI_Callback(uint16 GPIO_Pin)
 }
 
 //////////////////////////////////////////////////////////////////////
+// user_main() is called after HW init is complete
 
-extern "C" void begin()
+extern "C" void user_main()
 {
     HAL_TIM_Base_Start_IT(&htim17);
     max7219_init(&hspi1);
-}
 
-//////////////////////////////////////////////////////////////////////
-
-struct button_state_t
-{
-    int held;
-    int previous;
-    int pressed;
-    int released;
-
-    void update(int new_state)
+    while(true)
     {
-        held = new_state;
-        int change = held ^ previous;
-        previous = held;
-        pressed = change && held;
-        released = change && !held;
+        // sleep until an ISR has fired
+        __WFI();
+
+        button_state.update(button.down);
+
+        // debug button state on DEBUG1
+        DEBUG1_GPIO_Port->BSRR = DEBUG1_Pin << (button_state.held * 16);
+
+        // button toggles relay
+        if(button_state.pressed != 0)
+        {
+            MOSFET_GPIO_Port->ODR ^= MOSFET_Pin;
+        }
+
+        // pulse display brightness
+        int i = min(7, max(-7, abs((((int)ticks >> 12) & 63) - 32) - 16)) + 8;
+        max7219_set_intensity(i);
+
+        // update number
+        int r = atomic_read_clear(&rotation);
+        if(r != 0)
+        {
+            number += r;
+            if(number < 0)
+            {
+                number += 10000;
+            }
+            else if(number >= 10000)
+            {
+                number -= 10000;
+            }
+            max7219_set_number(number);
+        }
+        max7219_update();
     }
-};
-
-button_state_t button_state;
-
-//////////////////////////////////////////////////////////////////////
-
-extern "C" void loop()
-{
-    __WFI();
-
-    button_state.update(button.down);
-
-    DEBUG1_GPIO_Port->BSRR = DEBUG1_Pin << (button_state.held * 16);
-
-    if(button_state.pressed != 0)
-    {
-        MOSFET_GPIO_Port->ODR ^= MOSFET_Pin;
-    }
-
-    int i = min(7, max(-7, abs((((int)ticks >> 12) & 63) - 32) - 16)) + 8;
-
-    max7219_set_intensity(i);
-
-    number += rotation;
-    rotation = 0;
-    if(number < 0)
-    {
-        number += 10000;
-    }
-    else if(number >= 10000)
-    {
-        number -= 10000;
-    }
-
-    if(display_number != number)
-    {
-        display_number = number;
-        max7219_set_number(number);
-    }
-
-    max7219_update();
 }
