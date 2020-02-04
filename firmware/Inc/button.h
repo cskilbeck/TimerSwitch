f75cedc01ab46704585e2b8a5c35acfff2d0ca0a
Charlie <charlie>
2020-02-04 10:52:15 +0000

    cpp

--- firmware/Inc/button.h
+++ firmware/Inc/button.h
@@ -4,23 +4,37 @@
 
 //////////////////////////////////////////////////////////////////////
 
-struct button_t
+template <uintptr gpio_port, int bit_num, int history_len> struct button_t
 {
-    button_t(GPIO_TypeDef *_port, int _bit_num);
+    static_assert(history_len <= 32 && history_len >= 2, "Must use 2..32 for history");
 
     // call this at, say, 10KHz
-    void update();
+    void update()
+    {
+        // for masking off old ones readings
+        constexpr uint32 mask = static_cast<uint32>((1llu << history_len) - 1);
 
-    // where to get button state from
-    GPIO_TypeDef *port;
-    int           bit_num;
+        // can't use a ptr as a template arg so cast
+        auto port = reinterpret_cast<volatile GPIO_TypeDef *>(gpio_port);
+
+        // get gpio state
+        int bit = ((port->IDR) >> bit_num) & 1;
+
+        // shift into history, check for a run of ones
+        history = ((history << 1) | bit) & mask;
+        bool on = (history & (history + 1)) != 0;
+
+        // alternative history, check for a run of zeros
+        history = ~history & mask;
+        bool off = (history & (history + 1)) != 0;
+
+        // update button state
+        down = int((down || on) && !off);
+    }
 
     // track last 32 states
     uint32 history{ 0 };
 
-    // tracking admin
-    bool held{ false };
-    bool prev{ false };
-    bool pressed{ false };
-    bool released{ false };
+    // current status of button, 0 or 1
+    volatile int down;
 };
--- firmware/Src/button.cpp
+++ firmware/Src/button.cpp
@@ -1,32 +1,4 @@
 //////////////////////////////////////////////////////////////////////
 
+#include "main.h"
 #include "button.h"
-
-//////////////////////////////////////////////////////////////////////
-
-button_t::button_t(GPIO_TypeDef *_port, int _bit_num) : port(_port), bit_num(_bit_num)
-{
-}
-
-//////////////////////////////////////////////////////////////////////
-
-void button_t::update()
-{
-    // bitmap of 32 recent readings
-    int bit = ((_port->IDR) >> bit_num) & 1;
-    history = (history << 1) | bit;
-
-    // offs followed by an on = a button down event
-    // ons followed by an off = a button up event
-    bool press = (history & (history + 1)) != 0;
-    bool release = (~history & (~history + 1)) != 0;
-
-    // update button held state
-    held = (held || press) && !release;
-
-    // track changes (pressed, released)
-    bool diff = held != prev;
-    prev = held;
-    pressed = pressed || (diff && held);
-    released = released || (diff && !held);
-}
